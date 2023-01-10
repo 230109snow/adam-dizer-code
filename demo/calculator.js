98cb593c29c398e93127693fbe9e3e8d81def7ea
adamdizer <119471250+adamdizer@users.noreply.github.com>
2023-01-10 17:06:37 -0600

    started working on the calculator

--- demo/calculator.js
+++ demo/calculator.js
@@ -4,7 +4,57 @@
 Create a calculator app that can perform simple calculation- with ui!
 */
 
-function calculator(value)
+let previousVal = null;
+let operatorVal = null;
+let currentVal = null;
+let equationVal = null;
+
+function onNumberClick(value)
 {
-    //switch
+
+
+
+    switch(value)
+    {
+        case '9':
+            break;
+        case '8':
+            break;
+        case '7':
+            break;
+        case '6':
+            break;
+        case '5':
+            break;
+        case '4':
+            break;
+        case '3':
+            break;
+        case '2':
+            break;
+        case '1':
+            break;
+        case '+':
+            operatorVal = value;
+            break;
+        case '-':
+            operatorVal = value;
+            break;
+        case '/':
+            operatorVal = value;
+            break;
+        case 'x':
+            operatorVal = value;
+            break;
+        case '=':
+
+            break;
+        default:
+
+    }
+
+    console.log(previousVal, operatorVal, currentVal, equationVal);
+    
+
+    document.getElementById('expression').innerText = equationVal;
 }
