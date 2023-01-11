7840cff2ae49acaba31610137a84be83b1c7ff38
adamdizer <119471250+adamdizer@users.noreply.github.com>
2023-01-11 16:01:35 -0600

    first iteration of the calculator

--- demo/calculator.js
+++ demo/calculator.js
@@ -4,60 +4,111 @@
 Create a calculator app that can perform simple calculation- with ui!
 */
 
-let previousVal = null;
+let previousVal = NaN;
 let operatorVal = null;
-let currentVal = null;
-let equationVal = null;
+let currentVal = NaN;
+let equationVal = false;
+
+
+
+function clearMemory()
+{
+    previousVal = NaN;
+    operatorVal = null;
+    currentVal = NaN;
+    equationVal = false;
+}
+
+function calculate(a,operator,b)
+{
+    let returnVal = NaN;
+
+    switch(operator)
+    {
+        case '+':
+            returnVal = a + b;
+            break;
+        case '-':
+            returnVal = a - b;
+            break;
+        case '/':
+            if (b != 0)
+                returnVal = a / b;
+            break;
+        case 'x':
+            returnVal = a * b;
+            break;
+        default:
+    }   
+
+    return returnVal 
+}
+
+function updateDisplayValues(pVal, oVal, cVal)
+{
+    let returnString = "";
+
+    // case of if we have have one number entered and a operand
+    if (isNaN(pVal) && oVal != null)
+    {
+        // place the operand after the last value for display friendliness
+        returnString += isNaN(cVal) ? " " : cVal;
+        returnString += oVal == null ? " " : oVal;   
+    }
+    else
+    {
+        // Set the first value
+        returnString += isNaN(pVal) ? " " : pVal;   
+        // Set the operand
+        returnString += oVal == null ? " " : oVal;   
+        // set the second value
+        returnString += isNaN(cVal) ? " " : cVal;
+    }
+
+    return returnString;
+
+}
 
 function onClick(value)
 {
+    console.log(value,"|", previousVal, operatorVal, currentVal, equationVal);
 
+    // declare local variables
+    const displayTag = document.getElementById('expression');
+    let displayString = "";
 
-
-    switch(value)
+    // check the incoming value
+    if (value === 'c')
+        clearMemory(); // clear the memory slots
+    else if (value === '=')
+        equationVal = true; // flag to be calculated
+    else if (value === '+' ||
+            value === '-' ||
+            value === '/' ||
+            value === 'x')
+        operatorVal = String(value); // assign the arithmatic operation to be performed
+    else
     {
-        case '9':
-            if (previousVal == null)
-                previousVal = value;
-            break;
-        case '8':
-            break;
-        case '7':
-            break;
-        case '6':
-            break;
-        case '5':
-            break;
-        case '4':
-            break;
-        case '3':
-            break;
-        case '2':
-            break;
-        case '1':
-            break;
-        case '+':
-            operatorVal = value;
-            break;
-        case '-':
-            operatorVal = value;
-            break;
-        case '/':
-            operatorVal = value;
-            break;
-        case 'x':
-            operatorVal = value;
-            break;
-        case '=':
+        // ***********************************
+        // THIS IS SIMPLE SINGLE DIGIT LOGIC 
+        // THIS NEEDS TO BE REWORKED FOR 
+        // MULTI-DIGIT NUMBERS 
+        // ***********************************
 
-            break;
-        default:
+        // shift the value over
+        if ((isNaN(previousVal) && !isNaN(currentVal)) ||
+                (!isNaN(previousVal) && !isNaN(currentVal)))
+                    previousVal = currentVal; 
 
+        // set the new value
+        currentVal = value; 
     }
 
-    console.log(previousVal, operatorVal, currentVal, equationVal);
-    
+    if (equationVal === true)
+        displayString = calculate(previousVal, operatorVal, currentVal);
+    else
+        displayString = updateDisplayValues(previousVal, operatorVal, currentVal);
 
-    document.getElementById('expression').innerText = value;
-    //document.getElementById('expression').innerText = equationVal;
+    displayTag.innerText = displayString; 
+
 }
