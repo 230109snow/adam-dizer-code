1068335c23512dd665c9d59d717af6174feaf79c
adamdizer <119471250+adamdizer@users.noreply.github.com>
2023-01-12 17:03:10 -0600

    started working on the catapi

---
+++ demo/catapi.js
@@ -0,0 +1,78 @@
+
+let apiKey, apiKeyLoaded = false; 
+fetch('./apiKey.json').then((res) => res.json()).then((data) => {
+    
+    //apiKey = data.TheCatAPI;
+    apiKeyLoaded = true;
+    //console.log(data);
+})
+
+
+let isFirstLoad = true;
+
+function loadForm()
+{
+    if (isFirstLoad)
+    {
+        getBreeds();
+        setCounter();
+    }
+    isFirstLoad = false;
+}
+
+
+function setCounter()
+{
+
+    let numMax = 30;
+    const breed = document.getElementById('breed').value;
+    const numCats = document.getElementById('img-num');
+    
+    
+    for (i=1; i<=numMax; i++)
+    {
+        const numOption = document.createElement('option');
+        numOption.value = i;
+        numOption.innerText = i;
+        numCats.appendChild(numOption);
+    }
+
+}
+
+function getBreeds()
+{
+    const catArray = fetch('https://api.thecatapi.com/v1/breeds').then((res) => res.json()).then((data) => {
+        
+    const catBreed = document.getElementById('breed');
+
+        for (let i=0; i<data.length; i++)
+        {
+            const catOption = document.createElement('option');
+            catOption.innerText = data[i].name;
+            catOption.id = data[i].id;
+            catBreed.appendChild(catOption);
+        }
+    
+    })
+
+    
+}
+
+function getCats()
+{
+    if(apiKeyLoaded) 
+    {
+        const numCats = document.getElementById('img-num');
+        const breed = document.getElementById('breed');
+        const form = document.getElementById('cat-api-form');
+        const breedTest = document.getElementsByName(document.getElementById('breed').value).id
+
+
+        console.log(numCats)
+        console.log(breed.childNodes)
+        console.log(breedTest)
+
+        const url = breed ? `https://api.thecatapi.com/v1/images/search?limit=${numCats}&breed_ids=${breed}` : 
+                            `https://api.thecatapi.com/v1/images/search?limit=${numCats}`;
+    }
+}
